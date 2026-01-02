708ae1f5fc34bfdc88c462acc16342df09997353
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-01-02 13:11:48 +0500

    .

--- index.js
+++ index.js
@@ -5,23 +5,37 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    showCountries(data);
+    products = data;
+    showCountries(products);
   });
 
 function showCountries(data) {
-  data.map((item) => {
-    const instruction = document.getElementById("instruction");
+  const countries = document.getElementById("countries");
+  countries.innerHTML = "";
 
-  instruction.innerHTML += `
-  <div class="info">
-    <h3>How to use Country Explorer?</h3>
-    <ol>
-      <li>Search country name in the input</li>
-      <li>Click the "Choose" button</li>
-      <li>See country information below</li>
-    </ol>
-  </div>
-`;
+  data.map((item) => {
+    countries.innerHTML += `
+      <div class="card">
+        <img src="${item.flags.png}" alt="${item.name.common}">
+        <h3>${item.name.common}</h3>
+        <p><b>Capital:</b> ${item.capital ? item.capital[0] : "No capital"}</p>
+        <p><b>Region:</b> ${item.region}</p>
+        <p><b>Population:</b> ${item.population}</p>
+      </div>
+    `;
   });
 }
 
+
+const input = document.querySelector("input");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  const value = input.value.toLowerCase();
+
+  const filtered = products.filter((item) =>
+    item.name.common.toLowerCase().includes(value)
+  );
+
+  showCountries(filtered);
+});
